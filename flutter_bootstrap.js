d407f24ef2f5fcace92ad708caaad6a6203ca6f5
Arturo Sosa <arturososabagnoli@gmail.com>
2025-04-24 15:58:52 -0300

    realease v1.0.3.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "123694074"
+    serviceWorkerVersion: "3684113323"
   }
 });
